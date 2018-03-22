f7a253045317a1b28d352fa262eba0ab0043c4bf
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-22 23:14:48 +1300

    tests for string utils

--- test/utils.js
+++ test/utils.js
@@ -7,6 +7,24 @@ var strings = [
   'foo', 'bar', 'foo_', 'foo-', '', '', 'bar?','ba', 'baar'
 ]
 
+tape('is_string', function (t) {
+  t.equal(l.is_string(l.string(5)), 1)
+  t.equal(l.is_cons(l.string(5)), 0)
+  t.equal(l.is_string(l.cons(0, 0)), 0)
+  t.equal(l.is_cons(l.cons(0, 0)), 1)
+
+  t.end()
+})
+
+tape('string_length', function (t) {
+  var s = l.string(5)
+  console.log(s)
+  t.equal(l.string_length(s), 5)
+
+  t.end()
+
+})
+
 tape('string equals', function (t) {
   strings.forEach(function (a) {
     strings.forEach(function (b) {
@@ -15,5 +33,8 @@ tape('string equals', function (t) {
   })
   t.end()
 })
+
+
+
 
 
