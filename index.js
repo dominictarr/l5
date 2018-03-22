2f9cad021fa80b2cce0d41a00b411ae0133ebcc9
Dominic Tarr <dominic.tarr@gmail.com>
2018-03-22 18:22:37 +1300

    provide a log import

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var fs = require('fs'), path = require('path')
 var wasm = fs.readFileSync(path.join(__dirname, './l5.wasm'))
 
 var m = WebAssembly.Module(wasm)
-var instance = WebAssembly.Instance(m)
+var instance = WebAssembly.Instance(m, {console: {
+  log: function (n) { console.log("LOG", n, String.fromCodePoint(n)) }
+}})
 
 var buffer = new Buffer(instance.exports.memory.buffer)
 
@@ -22,20 +24,5 @@ exports.read = function (str) {
   var b = new Buffer(len)
   return buffer.slice(str+4, str+len+4).toString('utf8')
 }
-
-//var s1 = exports.write("***hello world***")
-//var s2 = exports.write("more strings")
-//console.log(exports.read(s1))
-//console.log(exports.read(s2))
-//var c2 = exports.cons(s2, 0)
-//var c1 = exports.cons(s1, c2)
-//
-//console.log('c2', c2)
-//console.log('c1', c1)
-//console.log(exports.is_cons(c1))
-//console.log(exports.head(c1))
-//console.log(exports.read(exports.head(c1)))
-//console.log(exports.read(exports.head(exports.tail(c1))))
-//
 
 
